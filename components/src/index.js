28563fba8ad86466a8df50943c590b3310ff6687
atsushi.koga <stbe51@gmail.com>
2019-09-21 22:45:52 +0900

    create first try

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    // propsを使って各Rowコンポーネントに情報を渡す
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <>
+            <Row name="Alex" time="Today at 5:00" text="1" />
+            <Row name="Jane" time="Today at 6:00" text="2" />
+            <Row name="Sum" time="Today at 7:00" text="3" />
+        </>
+    );
+};
+
+const Row = (props) => {
+    const rowStyle = {display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', alignItems: 'center', justifyContent: 'flex-start', margin: '10px 0 10px 30px'};
+    const iconStyle = {width: '100px', height: '100px', backgroundColor: 'blue', marginRight: '10px'};
+    const infoBox = {width: '200px', height: '100px', backgroundColor: 'red'};
+    const infoDetailStyle = {display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', alignItems: 'center', justifyContent: 'space-between'};
+
+    return (
+        <div style={rowStyle}>
+            <div style={iconStyle}>image</div>
+            <div style={infoBox}>
+                <div style={infoDetailStyle}>
+                    <div>{props.name}</div>
+                    <div>{props.time}</div>
+                </div>
+                <div>{props.text}</div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
