b15210ee262e0362fd7d3a68839d0ec9cc0df6cc
atsushi.koga <stbe51@gmail.com>
2019-09-23 14:01:54 +0900

    create geolocation API in App Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,17 +7,39 @@ import ReactDOM from 'react-dom';
  * 2. React.Componentを継承している事
  * 3. JSXを返すrenderメソッドを定義している事
  *
+ * 【state】
+ * ・stateはコンポーネントに関連したデータを含むJSオブジェクト
+ * ・stateを更新すると、ひもづくコンポーネントはすぐに再レンダリングされる（たいていの場合）
+ * ・stateはコンポーネントが最初に生成された時に、初期化されなければならない
+ * ・stateは'setState'メソッドを通じてのみ更新可能
+ * ・'setState'メソッドには、追加・更新したいプロパティのみをセットすれば良い（変更を行わないプロパティは記述不要）
  */
 class App extends React.Component {
-    render() {
-        console.log('start geo');
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
+    constructor(props) {
+        super(props);
 
-        console.log('start rendering');
-        return <div>Latitude: test</div>
+        this.state = {lat: null, errorMessage: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({errorMessage: err.message})
+            }
+        );
+    }
+
+    // stateが更新されるたびに実行される
+    render() {
+        if(this.state.lat){
+            return (<div>Latitude: {this.state.lat}</div>); 
+        }
+        if(this.state.errorMessage){
+            return (<div>Error: {this.state.errorMessage}</div>);
+        }
+
+        return (<div>loading...</div>);
     }
 }
 
