fed0905e80c3ed256fc40c240415b93e47c81848
atsushi.koga <stbe51@gmail.com>
2019-09-28 11:57:43 +0900

    pics: fetch images from splash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends React.Component {
+    state = {images: []};
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 39918a48868eaf3273dc0475feb8e61816a80ff68a3a84a1db592762bbdfa5b7'
+            }
+        });
+
+        this.setState({images: response.data.results});
+        console.log(response.data.results);
+    };
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
+            </div>
+        );
+    }
+}
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -8,17 +8,25 @@ import React from 'react';
  *
  */
 class SearchBar extends React.Component {
-    state = {term: ''};
+    state = {term: 'aaaa'};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    };
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="ui field">
                         <label>Image Search</label>
                         <input type="text"
                                value={this.state.term}
-                               onChange={(e) => {this.setState({term: e.target.value})}}
+                               onChange={(e) => {
+                                   this.setState({term: e.target.value})
+                               }}
                         />
                     </div>
                 </form>
